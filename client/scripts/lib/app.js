fe2a12d645b2a27192e06b2ab821b1de06109cb3
Marc Simonton <marcasimonton@gmail.com>
2016-11-01 14:45:11 -0600

    modals

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -16,7 +16,44 @@ const App = 'Farmshare';
 Angular.module(App, [
   'angular-meteor',
   'ionic'
-]);
+])
+
+
+
+.controller('MyController', function($scope, $ionicModal) {
+  $scope.modalClasses = ['slide-in-up', 'slide-in-down', 'fade-in-scale', 'fade-in-right', 'fade-in-left', 'newspaper', 'jelly', 'road-runner', 'splat', 'spin', 'swoosh', 'fold-unfold'];
+
+  $scope.openModal = function(animation) {
+    $ionicModal.fromTemplateUrl('my-modal.html', {
+      scope: $scope,
+      animation: animation
+    }).then(function(modal) {
+      $scope.modal = modal;
+      $scope.modal.show();
+    });
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+});
+
+
+
+
+
+
 
 new Loader(App)
   .load(signup)
@@ -33,6 +70,15 @@ else {
 function onReady() {
   Angular.bootstrap(document, [App]);
 }
+
+
+
+
+
+
+
+
+
 
 // Parallax for landing page
 // var jumboHeight = $('.customJumbotron').outerHeight();
